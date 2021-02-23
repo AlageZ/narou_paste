e432eb46c643c81190fe8a7994143a610b54f96f
AlageZ <76018036+AlageZ@users.noreply.github.com>
2021-02-23 13:31:55 +0900

    Delete naroupaste directory

--- naroupaste/main.js
+++
@@ -1,7 +0,0 @@
-function lr_p(e){
-    let con = document.getElementById("htmldiv")
-    let txt = document.getElementById("txtarea")
-    txt.value = con.innerHTML.toString().replace(/<ruby [^>]*>/g, "|").replace(/<rt [^>]*>/g, "《").replace(/<\/rt>/g, "》").replace(/<br>/g,"\n").replace(/<\/p>/g,"\n").replace(/<[^>]*>/g,"").replace(/&lt;/g, '<').replace(/&gt;/g, '>').replace(/&quot;/g, '"').replace(/&amp;/g, '&')
-
-
-}
