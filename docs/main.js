e667b5c08c80cc9226f905c28589abfce30d97ac
AlageZ <76018036+AlageZ@users.noreply.github.com>
2021-02-25 23:37:10 +0900

    Add files via upload

--- docs/main.js
+++ docs/main.js
@@ -1,7 +1,12 @@
 function lr_p(e){
     let con = document.getElementById("htmldiv")
     let txt = document.getElementById("txtarea")
-    txt.value = con.innerHTML.toString().replace(/<ruby [^>]*>/g, "|").replace(/<rt [^>]*>/g, "《").replace(/<\/rt>/g, "》").replace(/<br>/g,"\n").replace(/<\/p>/g,"\n").replace(/<[^>]*>/g,"").replace(/&lt;/g, '<').replace(/&gt;/g, '>').replace(/&quot;/g, '"').replace(/&amp;/g, '&')
-
-
+    txt.value = con.innerHTML.toString().replace(/\n/g,"").replace(/<ruby[^>]*>/g, "|").replace(/<rt[^>]*>/g, "《").replace(/<\/rt>/g, "》").replace(/<br>/g,"\n").replace(/<\/p>/g,"\n").replace(/<[^>]*>/g,"").replace(/&lt;/g, '<').replace(/&gt;/g, '>').replace(/&quot;/g, '"').replace(/&amp;/g, '&')
+}
+function rl_p(e){
+    let con = document.getElementById("htmldiv")
+    let txtd = document.getElementById("txtarea").value.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, "&quot;")
+    txtd = txtd.replace(/(｜|\|)([^《]*)《([^》]*)》/g,"<ruby>$2<rt>$3</rt></ruby>")
+    txtd = txtd.replace(/^/gm,"<p>").replace(/$/gm,"</p>")
+    con.innerHTML = txtd
 }
